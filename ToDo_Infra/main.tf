9cc7dbef77586b7e1ef06d5b35d12ad9e0effb85
Raushan Kumar <raushankr28@gmail.com>
2026-02-04 13:36:36 +0530

    add rg2

--- ToDo_Infra/main.tf
+++ ToDo_Infra/main.tf
@@ -13,6 +13,12 @@ module "resource_group" {
   resource_group_location = "East US"
 }
 
+module "resource_group2" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-todoapp1"
+  resource_group_location = "East US"
+}
+
 module "virtual_network" {
   depends_on = [module.resource_group]
   source     = "../modules/azurerm_virtual_network"

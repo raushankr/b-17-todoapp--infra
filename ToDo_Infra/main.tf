9e9d23840b0bd3461c83f8219fbd587a0f3ff49a
Raushan Kumar <raushankr28@gmail.com>
2026-02-04 18:12:56 +0530

    vnet dhondhu added

--- ToDo_Infra/main.tf
+++ ToDo_Infra/main.tf
@@ -27,6 +27,14 @@ module "virtual_network" {
   resource_group_name      = "rg-todoapp1"
   address_space            = ["10.0.0.0/16"]
 }
+module "virtual_network" {
+  depends_on = [module.resource_group]
+  source     = "../modules/azurerm_virtual_network"
+  virtual_network_name     = "vnet-dhondhu"
+  virtual_network_location = "East US"
+  resource_group_name      = "rg-todoapp1"
+  address_space            = ["10.0.0.0/16"]
+}
 
 module "frontend_subnet" {
   depends_on = [module.virtual_network]
